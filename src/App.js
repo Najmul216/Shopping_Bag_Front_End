240dcfe563abf29fa676d555bdd8e292750278da
Najmul Hasan Chowdhury <hnajmul39@gmail.com>
2021-05-24 02:02:50 +0800

    Navbar Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import NavbarContainer from './components/layout/Navbar/NavbarContainer';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <NavbarContainer />
     </div>
   );
 }
