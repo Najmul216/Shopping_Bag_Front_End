8feb2cae918031100558da53a5f14cd32e8d4d07
Najmul Hasan Chowdhury <hnajmul39@gmail.com>
2021-05-29 15:09:26 +0800

    React router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 //import './App.css';
 import LandingPage from './components/layout/LandingPage';
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 import NavbarContainer from './components/layout/Navbar/NavbarContainer';
 import Footer from './components/layout/Footer';
 import LoginContainer from './components/Login/LoginContainer';
@@ -13,13 +18,20 @@ function App() {
 
 
   return (
-    <div>
-      {/* <LandingPage t={t} i18n={i18n} /> */}
+    <Router>
+      <div>
 
-      <NavbarContainer t={t} i18n={i18n} />
-      <LoginContainer t={t} i18n={i18n} />
-      <Footer t={t} i18n={i18n} />
-    </div>
+        <NavbarContainer t={t} i18n={i18n} />
+          <Route exact path="/">
+            <LandingPage t={t} i18n={i18n} />
+          </Route>
+          <Route exact path="/login" >
+            <LoginContainer t={t} i18n={i18n} />
+          </Route>`
+
+        <Footer t={t} i18n={i18n} />
+      </div>
+    </Router>
   );
 }
 
--- src/components/layout/LandingPage.js
+++ src/components/layout/LandingPage.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import HomePageProductContainer from '../HomePageProduct/HomePageProductContainer';
-import Footer from './Footer';
 import ImageSlider from './ImageSlider';
-import NavbarContainer from './Navbar/NavbarContainer';
+
 
 function LandingPage(props) {
 
     return (
         <React.Fragment>
-            <NavbarContainer {...props} />
             <div className="container-fluid px-0">
                 <ImageSlider />
                 <HomePageProductContainer />
-                <Footer {...props} />
             </div>
         </React.Fragment>
     )
